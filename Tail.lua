ea7fba2da2ce620afb2d1818635b249a614e8f10
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-24 21:56:39 +0000

    Update Tail.lua

--- Tail.lua
+++ Tail.lua
@@ -1,9 +1,18 @@
 print("load")
+local MarketPlaceService = game:GetService("MarketplaceService")
 local function newplayer(char)
 	local item 
 	local TweenService = game:GetService("TweenService")
 	for i,v in pairs(char:GetChildren()) do
-		--print(v.Name)
+		print(v.Name)
+		if tonumber(v.Name) == v.Name then -- for games that have the name as a id (e.g funky friday)
+			local success, asset = pcall(MarketPlaceService.GetProductInfo,MarketPlaceService,v.Name)
+			if success then
+			if string.find(asset.Name:lower(),"tail") then
+				item = v	
+				end
+			end
+		end
 		if string.find(v.Name:lower(),"tail") then
 			item = v	
 		end
@@ -16,7 +25,7 @@ local function newplayer(char)
 	M6D.Part0 = item.Handle.AccessoryWeld.Part0
 	M6D.Part1 = item.Handle.AccessoryWeld.Part1
 	local o = item.Handle.AccessoryWeld.C0.Position
-	M6D.C0 = CFrame.new(o)
+	M6D.C0 = CFrame.new(o) * CFrame.Angles(0,50,0)
 	item.Handle.AccessoryWeld:Destroy()
 	while task.wait() do
 		local TweenService = game:GetService("TweenService")
@@ -29,7 +38,7 @@ local function newplayer(char)
 	end
 end
 game.Players.LocalPlayer.CharacterAdded:Connect(function()
-	--print("char added")
+	print("char added")
 	task.wait(1)
 	newplayer(game.Players.LocalPlayer.Character) 
 end)
