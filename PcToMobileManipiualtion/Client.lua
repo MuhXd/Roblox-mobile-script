3ad15461a0597943a77ef1607e9c36404e63336b
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-28 14:13:10 +0000

    Update Client.lua

--- PcToMobileManipiualtion/Client.lua
+++ PcToMobileManipiualtion/Client.lua
@@ -1,27 +1,21 @@
 if not game:IsLoaded() then 
-    game.Loaded:Wait() 
+	game.Loaded:Wait() 
 end
 
 local websocket
 
-local serverIP = _G.PCHOOKIP or "192.168.1.131" -- Replace this with the actual IP address of your WebSocket server
-local serverPort = _G.PCHOOKPORT or 1000
+local serverIP = _G.PCHOOKIP or "192.168.1.131" -- this is connect to pc on the same network
+local serverPort = _G.PCHOOKPORT or 1000 -- opened port
 
 
-if not game:IsLoaded() then game.Loaded:Wait() end
 local web
 repeat wait() until pcall(function() web =  WebSocket.connect("ws://"..serverIP..":"..serverPort) end) == true
 
-if not _G.autoran then
-    web:Send("autoexec")
-   _G.autoran = true 
+if not _G.PCHOOKautoran then
+	--web:Send("autoexec")
+	_G.PCHOOKautoran = true 
 end
 
 web.OnMessage:Connect(function(msg)
-    s,e = pcall(function()
-       assert(loadstring(msg))()
-    end)
-    if not s then
-    error(e)
-    end
+	assert(loadstring(tostring(msg)))()
 end)
