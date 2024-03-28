94c7e9d857b0af7357947f88927c7d65f164bc47
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-28 22:21:25 +0000

    Update Client.lua

--- PcToMobileManipiualtion/Client.lua
+++ PcToMobileManipiualtion/Client.lua
@@ -11,5 +11,17 @@ web = WebSocket.connect("ws://"..serverIP..":1000")
 web.OnMessage:Connect(function(Msg)
 	assert(loadstring(tostring(Msg)))()
 end)
+
 print(" -- quick reminder that you can show the ip as it is a inhouse network ip so it won't be able to be connected to unless you are in the same network -- ")
 print("Successfully Opened WebSocket at: "..serverIP)
+
+local oldprint; oldprint = hookfunction(warn, function(...)
+	args = {...}
+	web.send("Warn: "..arg[1])
+	return oldprint(...);
+end);
+local oldprint; oldprint = hookfunction(print, function(...)
+	args = {...}
+	web.send("Print: "..arg[1])
+	return oldprint(...);
+end);
