063fbb8bba457c6a61edd5d050aac6518e5fd1d9
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-28 14:17:36 +0000

    Update Client.lua

--- PcToMobileManipiualtion/Client.lua
+++ PcToMobileManipiualtion/Client.lua
@@ -5,17 +5,9 @@ end
 local websocket
 
 local serverIP = _G.PCHOOKIP or "192.168.1.131" -- this is connect to pc on the same network
-local serverPort = _G.PCHOOKPORT or 1000 -- opened port
 
 
-local web
-repeat wait() until pcall(function() web =  WebSocket.connect("ws://"..serverIP..":"..serverPort) end) == true
-
-if not _G.PCHOOKautoran then
-	--web:Send("autoexec")
-	_G.PCHOOKautoran = true 
-end
-
-web.OnMessage:Connect(function(msg)
-	assert(loadstring(tostring(msg)))()
+web = WebSocket.connect("ws://"..serverIP..":1000") 
+web.OnMessage:Connect(function(Msg)
+	assert(loadstring(tostring(Msg)))()
 end)
