cf691e94e287e1b7a97058e562cd509bd10c7045
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-28 10:49:08 +0000

    Update Client.lua

--- PcToMobileManipiualtion/Client.lua
+++ PcToMobileManipiualtion/Client.lua
@@ -1,9 +1,10 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
-local ws = syn and syn.websocket.connect or Krnl and Krnl.WebSocket.connect or WebSocket and WebSocket.connect or websocket and websocket.connect
-local web
-repeat wait() until pcall(function() web = ws("ws://localhost:1000") end) == true
+local websocket
+repeat wait() until pcall(function() websocket = WebSocket.connect("ws://localhost:1000") end) == true
 
 
-web.OnMessage:Connect(function(msg)
+websocket.OnMessage:Connect(function(msg)
     print(msg)
 end)
+
+
