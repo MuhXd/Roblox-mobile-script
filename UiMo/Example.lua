31d1a2addb939d27255591a3a3eec782592deef6
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-04-08 20:45:04 +0100

    Create Example.lua

---
+++ UiMo/Example.lua
@@ -0,0 +1,36 @@
+
+local Ui = loadstring(game:HttpGet("https://github.com/MuhXd/Roblox-mobile-script/blob/main/UiMo/Loader.lua?raw=true"))();
+
+Local MoUi = Ui.SpawnUi({
+	["UiName"] = "Hub",
+	["Deley"] = 3
+})
+
+
+MoUi.Button({
+	["Name"] = "Test Button",
+	["Info"] = "Test Info",
+	["InfoButton"] = "._.",
+	["Function"] = function ()
+			TEST.Notify({
+				["Name"] = "Test notify",
+				["Info"] = "Test info",
+				["LifeTime"] = 20
+			})
+		
+	end
+})
+
+MoUi.NewLine()
+
+MoUi.CheckBox({
+	["Name"] = "CheckBox alert",
+	["Info"] = "Template",
+	["Function"] = function (bool)
+		TEST.Notify({
+			["Name"] = "Test CheckBox",
+			["Info"] = "Enabled: "..tostring(bool),
+			["LifeTime"] = 20
+		})
+	end
+})
