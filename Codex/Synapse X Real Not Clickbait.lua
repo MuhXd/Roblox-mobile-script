7f523e0bef8bc801465aa729d62f61df956fb83e
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-04-11 21:51:33 +0100

    Update Synapse X Real Not Clickbait.lua

--- Codex/Synapse X Real Not Clickbait.lua
+++ Codex/Synapse X Real Not Clickbait.lua
@@ -1,25 +1,40 @@
-CodexUi = game:GetService("RunService"):IsStudio() and game.StarterGui.Codex or game:GetService("CoreGui").Codex
+local image = _G.CodexSpooferImage or 9524079125
+local text = _G.CodexSpooferText or "Synapse X"
+local powered = _G.CodexSpooferPoweredBy or "Powered by Nameless Admin"
+local Uis = {
+
+}
+CodexUi = game:GetService("RunService"):IsStudio() and game.StarterGui.Codex or game:GetService("CoreGui"):WaitForChild("Codex")
+if CodexUi:FindFirstChild("Codex"):FindFirstChild("gui") then
 -- codex
-CodexUi:WaitForChild("gui")
-local player = game.Players.LocalPlayer
-local mouse = player:GetMouse()
-oldm = mouse.Icon
+for i, v in pairs(CodexUi:GetChildren()) do
+	v.Parent = game.CoreGui
+	v:SetAttribute("Codex",true)
+	table.insert(Uis,v)
+end
+else
+	for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
+		if v:GetAttribute("Codex") then
+			table.insert(Uis,v)
+		end
+	end
+end
+
 function HideForever(object) 
 	object.Visible = false
 	object:GetPropertyChangedSignal("Visible"):Connect(function()
 		object.Visible = false
 	end)
 end
-HideForever(CodexUi.gui.fade)
-CodexUi:WaitForChild("gui"):WaitForChild("navbar")
-CodexUi.gui.tabs.editor.contentContainer.inputBox.MultiLine = true
-CodexUi.gui.navbar.floatingIcon.codexIcon2.Image = "http://www.roblox.com/asset/?id=9524079125"
-CodexUi.gui.navbar.main.codexIcon.Image = "http://www.roblox.com/asset/?id=9524079125"
-CodexUi.gui.navbar.main.title.Text = "Synapse X"
-CodexUi.gui.navbar.main.title:GetPropertyChangedSignal("Text"):Connect(function()
-	CodexUi.gui.navbar.main.title.Text = "Synapse X"
+HideForever(Uis.gui.fade)
+Uis.gui.tabs.editor.contentContainer.inputBox.MultiLine = true
+Uis.gui.navbar.floatingIcon.codexIcon2.Image = "http://www.roblox.com/asset/?id="..image
+Uis.gui.navbar.main.codexIcon.Image = "http://www.roblox.com/asset/?id="..image
+Uis.gui.navbar.main.title.Text = text
+Uis.gui.navbar.main.title:GetPropertyChangedSignal("Text"):Connect(function()
+	Uis.gui.navbar.main.title.Text = text
 end)
-CodexUi.gui.navbar.main.poweredBy:GetPropertyChangedSignal("Text"):Connect(function()
-		CodexUi.gui.navbar.main.poweredBy.Text = "Powered by Nameless Admin"
+Uis.gui.navbar.main.poweredBy:GetPropertyChangedSignal("Text"):Connect(function()
+	Uis.gui.navbar.main.poweredBy.Text = powered
 end)
-CodexUi.gui.navbar.main.poweredBy.Text = "Powered by Nameless Admin"
+Uis.gui.navbar.main.poweredBy.Text = powered
