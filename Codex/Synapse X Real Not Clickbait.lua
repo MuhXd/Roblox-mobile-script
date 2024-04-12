8fa94fde0751b2a179ff355c7da782f5cc0f42c4
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-04-12 02:08:29 +0100

    Update Synapse X Real Not Clickbait.lua

--- Codex/Synapse X Real Not Clickbait.lua
+++ Codex/Synapse X Real Not Clickbait.lua
@@ -3,39 +3,7 @@ local text = _G.CodexSpooferText or "Synapse X"
 local powered = _G.CodexSpooferPoweredBy or "Powered by Nameless Admin"
 local gui = _G.Codex_gui_Object or nil
 CodexUi = game:GetService("RunService"):IsStudio() and game.StarterGui.Codex or game:GetService("CoreGui"):WaitForChild("Codex")
-task.wait(1)
-if  gui == nil then
-if CodexUi:FindFirstChild("gui") then
--- codex
-for i, v in pairs(CodexUi:GetChildren()) do
-	v.Parent = game.CoreGui
-	v:SetAttribute("Codex",true)
-		if v.Name == "gui" then
-			gui = v
-		end
-end
-else
-	for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
-		if v:GetAttribute("Codex") then
-			if v.Name == "gui" then
-				gui = v
-			end
-		end
-	end
-	
-	if not gui then
-		for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
-			if v.Name == "gui" then
-				if v:FindFirstChild("tabs") then
-					if v:FindFirstChild("exploitSettings") then
-						gui = v
-					end
-				end
-		end
-	end
-end
-end
-end
+gui = CodexUi:FindFirstChild("gui")
 if not gui then
 warn("unable to find codex")
 end
