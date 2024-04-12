dbb65bb351aba201cb465c76690fe095f62fbbf7
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-04-12 01:13:23 +0100

    Update TrollModMenu.lua

--- Codex/TrollModMenu.lua
+++ Codex/TrollModMenu.lua
@@ -1,1 +1,12 @@
 
+
+local gui = _G.Codex_gui_Object or nil
+
+if not gui then
+	return warn("unable to find codex")
+end
+
+gui.navbar.floatingIcon.BackgroundColor3 = Color3.fromRGB(36, 102, 2)
+gui.navbar.floatingIcon.BorderColor = Color3.fromRGB(36, 102, 2)
+gui.navbar.floatingIcon.codexIcon2.Image = "rbxassetid://17108310457"
+-- codexIcon2
